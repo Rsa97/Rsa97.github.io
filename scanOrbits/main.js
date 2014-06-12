134422f4a4ed273736a157f26a1b3b16ff230871
Rsa97 <r.serge.a@gmail.com>
2014-06-12 20:27:19 +0400

    Scan orbit calculation - preAlpha

---
+++ scanOrbits/main.js
@@ -0,0 +1,149 @@
+/**
+ * @author Rsa
+ */
+
+function isPrime(m, n) {
+	if (m < 1 || n < 1 || m == n)
+		return false;
+	while (true) {
+		if (m == 0 || n == 0)
+			return (m == 1 || n == 1);
+		if (m == 1 || n == 1)
+			return true;
+		if (m == n)
+			return false;
+		if ((m&1) == 0 && (n&1) == 0)
+			return false;
+		else if ((m&1) == 0)
+			m >>= 1;
+		else if ((n&1) == 0)
+			n >>= 1;
+		else if (n > m)
+			n = (n-m)>>1;
+		else
+			m = (m-n)>>1;
+	}
+}
+
+function formatFloat(val, frac) {
+	val = Math.round(val*Math.pow(10, frac))/Math.pow(10, frac);
+	var i = Math.floor(val);
+	var res = ('00'+(i%1000)).substr(-3,3);
+	if (i < 1000)
+		res = i;
+	while ((i = Math.floor(i/1000)) > 0) {
+		res = (i < 1000 ? i : ('00'+(i%1000)).substr(-3,3))+' '+res;
+	}
+	if ((val = val-Math.floor(val)) != 0) 
+		res += (''+(Math.round(val*Math.pow(10, frac))/Math.pow(10, frac))).substr(1);
+	return res; 
+}
+
+function formatTime(period) {
+	period = Math.round(period*10)/10;
+	var days = Math.floor(period/21600);
+	var hours = Math.floor((period-days*21600)/3600);
+	var mins = Math.floor((period-days*21600-hours*3600)/60);
+	if (mins < 10)
+		mins = '0'+mins;
+	var secs = Math.floor((period-days*21600-hours*3600-mins*60)*10)/10;
+	if (secs < 10)
+		secs = '0'+secs;
+	return days+'.'+hours+':'+mins+":"+secs;
+}
+
+function calcOrbits() {
+	var minOver = 0.1;
+	var limitFOV = 1;
+	var maxInclin = 85/180/Math.PI;
+	var maxDays = 100;
+	var Alt, incPeriod, incAlt;
+	var minScanAlt = 0, maxScanAlt = 100000000;
+	var minResonance, maxResonance;
+	var bodyTurns, satTurns;
+	var minSatTurns = Math.ceil(360/totalMaxFOV*(minOver+1));
+	var maxSatTurns = Math.ceil(360/totalMinFOV*(minOver+1));
+	var bestResBody, bestResSat, minTime = 1000*6*60*60;
+	var minFOV, k;
+	var orbPeriod, orbAlt;
+	var done;
+	scanners.forEach(function(scanner, idx) {
+		if (scanner[scanAvail] == 0)
+			return;
+		if (minScanAlt < scanner[scanMinAlt])
+			minScanAlt = scanner[scanMinAlt];
+		if (maxScanAlt > scanner[scanMaxAlt])
+			maxScanAlt = scanner[scanMaxAlt];
+	});
+	celestialBodies.forEach(function(body, bodyIdx) {
+		if (bodyIdx == 0) // Skip the Sun
+			return;
+		minAlt = minScanAlt;
+		if (minAlt < body[cbPeak]+1000)
+			minAlt = body[cbPeak]+1000;
+		maxAlt = maxScanAlt;
+		if (maxAlt > body[cbSOI])
+			maxAlt = body[cbSOI];
+		incPeriod = Math.cos(maxInclin)*body[cbSideral];
+		incAlt = Math.pow(incPeriod*incPeriod/4/Math.PI/Math.PI*body[cbGM], 1/3.)-body[cbRadius];
+		if (maxAlt > incAlt)
+			maxAlt = incAlt;
+		minResonance = body[cbSideral]/(Math.sqrt(Math.pow(minAlt+body[cbRadius], 3)/body[cbGM])*2*Math.PI);
+		maxResonance = body[cbSideral]/(Math.sqrt(Math.pow(maxAlt+body[cbRadius], 3)/body[cbGM])*2*Math.PI);
+		console.log(body[cbName]+" : "+minAlt+" ("+Math.sqrt(Math.pow(minAlt+body[cbRadius], 3)/body[cbGM])*2*Math.PI+") : "+
+									   maxAlt+" ("+Math.sqrt(Math.pow(maxAlt+body[cbRadius], 3)/body[cbGM])*2*Math.PI+") : "+minResonance+" : "+maxResonance);
+		minTime = -1;
+		for (done = false, bodyTurns = 1; !done && bodyTurns <= maxDays; bodyTurns++) {
+			satTurns = Math.ceil(bodyTurns*maxResonance);
+			if (satTurns < minSatTurns)
+				satTurns = minSatTurns;
+			var maxSatTurns = Math.floor(bodyTurns*minResonance);
+			if (maxSatTurns > maxSatTurns*bodyTurns)
+				maxSatTurns = maxSatTurns*bodyTurns;
+//			console.log(bodyTurns+": "+satTurns+" -> "+maxSatTurns);
+			for (; !done && satTurns <= maxSatTurns; satTurns++)
+				if (isPrime(bodyTurns, satTurns)) {
+					orbPeriod = body[cbSideral]*bodyTurns/satTurns;
+					orbAlt = Math.pow(orbPeriod*orbPeriod/4/Math.PI/Math.PI*body[cbGM], 1/3.)-body[cbRadius];
+					if (orbAlt >= minAlt && orbAlt <= maxAlt) {
+						minFOV = totalMaxFOV;
+						scanners.forEach(function(scanner, scanIdx) {
+							var fov = getFOVbyAlt(bodyIdx, scanIdx, orbAlt);
+							if (minFOV > fov)
+								minFOV = fov;
+						});
+						k = 3-(bodyTurns&1)-(satTurns&1);
+						if (minFOV >= limitFOV && minFOV*satTurns*k >= 360*(1+minOver)) {
+							console.log(bodyTurns+"/"+satTurns+" : "+orbAlt+" : "+orbPeriod+" : "+minFOV+" : "+(minFOV*satTurns*k)/360+" : "+formatTime(orbPeriod*satTurns));
+							done = true;
+							break;
+						}
+					}
+				}
+		}
+	}); 
+}
+
+$(function() {
+	scanners.forEach(function(scanner, idx) {
+		$("#scanners").append('<tr data-idx="'+idx+'"><td><a class="btn btn-xs btn-success" data-toggle="dropdown" href="#">&nbsp;&nbsp;&nbsp;</a><td>'+scanner[scanName]+'<td>'+formatFloat(scanner[scanMinAlt], 1)+
+							 '<td>'+scanner[scanBestAlt]+'<td>'+scanner[scanMaxAlt]);	});
+	
+	calcOrbits();
+	
+	$("#scanners").on("click", ".btn", function() {
+		var row = $(this).parents("tr");
+		if (row.hasClass("unavail")) {
+			row.removeClass("unavail");
+			scanners[row.data("idx")][scanAvail] = 1;
+			$(this).removeClass("btn-danger").addClass("btn-success");
+		} else {
+			row.addClass("unavail");
+			scanners[row.data("idx")][scanAvail] = 0;
+			$(this).removeClass("btn-success").addClass("btn-danger");
+		}
+		setInterval(calcOrbits(), 100);
+	});
+	
+
+});
---
+++ scanOrbits/scanners.js
@@ -0,0 +1,42 @@
+var scanName = 0;
+var scanMinAlt = 1;
+var scanMaxAlt = 2;
+var scanBestAlt = 3;
+var scanFOV = 4;
+var scanPeriod = 5;
+var scanType = 6;
+var scanAvail = 7;
+
+var scanners = new Array(
+	new Array("SCAN RADAR Altimetry Sensor", 5000,  500000,    5000, 5,   0.5, 1, 1),
+	new Array("SCAN Multispectral Sensor",   5000,  500000,  250000, 4,   0.5, 1, 1),
+	new Array("SCAN SAR Altimetry Sensor",   5000,  800000,  750000, 2,   0.5, 1, 1),
+	new Array("KE-S210 Compact Survey Unit",    0,  250000, "любая", 2.5, 1.5, 2, 0),
+	new Array("KE-S110 Medium Survey Unit",     0, 1200000, "любая", 2.5, 0.9, 2, 1)
+);
+
+var totalMaxFOV = 41;
+var totalMinFOV = 1;
+
+function getFOVbyAlt(celestialBodyIdx, scannerIdx, altitude) {
+	if (altitude < scanners[scannerIdx][scanMinAlt] || 
+		altitude > scanners[scannerIdx][scanMaxAlt] ||
+		altitude < celestialBodies[celestialBodyIdx][cbPeak] ||
+		altitude > celestialBodies[celestialBodyIdx][cbSOI])
+		return 0.001;
+	switch (scanners[scannerIdx][scanType]) {
+		case 1: // ScanSat
+			var fov = scanners[scannerIdx][scanFOV]*(altitude < scanners[scannerIdx][scanBestAlt] ? (altitude/scanners[scannerIdx][scanBestAlt]) : 1)*
+					  (celestialBodies[celestialBodyIdx][cbRadius] > 600000 ? 1 : Math.sqrt(600000/celestialBodies[celestialBodyIdx][cbRadius]));
+			if (fov > 20)
+				fov = 20;
+			return Math.floor(fov)+Math.round(fov)+1; 
+			break;
+		case 2: // Kethane
+			return 2.5;
+			break;
+		default:
+			return 0.001;
+	}
+}
+
